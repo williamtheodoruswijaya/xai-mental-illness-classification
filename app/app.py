1cfad1be98eb9f6dc0dd28697677ce8c76f787fc
Admantix <williamwijaya499@gmail.com>
2025-06-15 20:18:42 +0700

    fix: Add log message to indicate dataset loading path

--- app/app.py
+++ app/app.py
@@ -42,6 +42,7 @@ def load_dataset() -> pd.DataFrame:
     try:
         BASE_DIR = os.path.dirname(__file__)
         DATA_PATH = os.path.join(BASE_DIR, '..', 'data', 'data.csv')
+        st.write(f"🔍 Mencoba memuat dataset dari: {DATA_PATH}")
         df = pd.read_csv("../data/data.csv", encoding='utf-8')
         df.columns = df.columns.str.strip()
         return df
